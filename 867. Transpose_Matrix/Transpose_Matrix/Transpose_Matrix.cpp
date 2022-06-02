734c62e6715e1502ace6889d0f589ef032d3f2b9
efobo <81191405+efobo@users.noreply.github.com>
2022-06-02 13:51:00 +0300

    new task

---
+++ 867. Transpose_Matrix/Transpose_Matrix/Transpose_Matrix.cpp
@@ -0,0 +1,56 @@
+﻿// Transpose Matrix
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+    int m = matrix.size();
+    int n = matrix[0].size();
+
+    vector<vector<int>> res;
+    vector<int> vec;
+
+        
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++) 
+        {
+            vec.push_back(matrix[j][i]);
+        }
+        res.push_back(vec);
+        vec.clear();
+    }
+    return res;
+}
+
+void printMatrix(vector<vector<int>>& matrix)
+{
+    int m = matrix.size();
+    int n = matrix[0].size();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+
+
+int main()
+{
+    vector<vector<int>> matrix = { {1, 2, 3},{4, 5, 6},{7, 8, 9} };
+    vector<vector<int>> ans = transpose(matrix);
+    printMatrix(ans);
+
+    cout << endl;
+
+    matrix = {{1, 2, 3}, { 4, 5, 6 }};
+    ans = transpose(matrix);
+    printMatrix(ans);
+}
